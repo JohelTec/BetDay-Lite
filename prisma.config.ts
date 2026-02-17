0d1cf68915f0c086e5e66b703a3df53682649f8f
Johel <johel.sistemas@gmail.com>
2026-02-17 14:52:32 -0500

    feat: changes

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // Aquí es donde Migrate buscará la base de datos
-    url: process.env.DATABASE_URL,
-  },
-});
